2998ac5775c9ead28431d2676046010d26005fc7
SIYANG TAO <siyang.tao@citrusad.com>
2018-07-04 14:03:15 +1000

    Add init test.

---
+++ tests/unit/testsuite/Citrus/Integration/controllers/Citrus_Integration_Controller_Queue_Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siyang
+ * Date: 3/07/18
+ * Time: 4:14 PM
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class Citrus_Integration_Controller_Queue_Test extends TestCase
+{
+    public function setUp()
+    {
+        $app = Mage::app('default');
+
+    }
+}
