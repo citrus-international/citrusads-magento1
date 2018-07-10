d61012f623f5525cd66e7798826b7a0efa50d28b
SIYANG TAO <siyang.tao@citrusad.com>
2018-07-10 12:18:15 +1000

    Fix the bug.

--- tests/unit/testsuite/Citrus/Integration/controllers/Citrus_Integration_Controller_Queue_Test.php
+++ tests/unit/testsuite/Citrus/Integration/controllers/Citrus_Integration_Controller_Queue_Test.php
@@ -18,70 +18,61 @@ class Citrus_Integration_Controller_Queue_Test extends TestCase
         $this->model = new Citrus_Integration_Adminhtml_Citrusintegration_QueueController;
     }
 
-    public function testProductAction() {
-        $allCollections = Mage::getModel('catalog/product')->getCollection()
-            ->addAttributeToSelect('*')
-            ->addAttributeToFilter('type_id', array('in' => array('simple', 'virtual')))
-            ->addAttributeToFilter('status', 1);
-        $count = count($allCollections);
+    // The method pushSyncItem() is protected
+    /*public function testPushSyncItem() {
+        $productCollections = $this->getProductsCollection();
 
-        // Add all products to queue
-        $this->mockProductAction();
+        $collections = Mage::getModel('citrusintegration/queue')->getCollection();
+        foreach ($collections as $item) {
+            $item->delete();
+        }
 
-        $queueCollection = Mage::getModel('citrusintegration/queue')->getCollection();
-        $this->assertEquals($count, count($queueCollection));
+        foreach ($productCollections as $collection) {
+            $this->model->pushItemToQueue($collection);
+        }
+
+        $catalog_product = Mage::getModel('citrusintegration/queue')->getCollection()
+            ->addFieldToSelect('entity_id')
+            ->addFieldToFilter('type', 'catalog/product')
+            ->setPageSize(100)
+            ->setCurPage(1);
+
+        $syncItems = new Varien_Object;
+        $syncItems->addData(array('catalog_product' => $catalog_product));
+        $return = $this->model->pushSyncItem($syncItems);
+
+        $this->assertTrue($return['success']);
+    }*/
+
+
+    public function testPushItemToQueue()
+    {
+        $productCollections = $this->getProductsCollection();
+        $count = count($productCollections);
+        var_dump($count);
+
+        $collections = Mage::getModel('citrusintegration/queue')->getCollection();
+        foreach ($collections as $item) {
+            $item->delete();
+        }
+        $this->assertEquals(0, Mage::getModel('citrusintegration/queue')->getCollection()->count());
+        var_dump($collections->load()->getSize());
+
+        foreach ($productCollections->getItems() as $collection) {
+            $this->model->pushItemToQueue($collection);
+        }
+
+        $this->assertEquals($count, Mage::getModel('citrusintegration/queue')->getCollection()->count());
     }
 
-//    public function testPushSyncItem() {
-//        // Add all products to queue
-//        $this->mockProductAction();
-//
-//        $catalog_product = Mage::getModel('citrusintegration/queue')->getCollection()
-//            ->addFieldToSelect('entity_id')
-//            ->addFieldToFilter('type', 'catalog/product')
-//            ->setPageSize(100)
-//            ->setCurPage(1);
-//
-//        $syncItems = new Varien_Object;
-//        $syncItems->addData(array('catalog_product' => $catalog_product));
-//        $return = $this->model->pushSyncItem($syncItems);
-//
-//        $this->assertTrue($return['success']);
-//    }
-
-
-    private function mockProductAction()
+    private function getProductsCollection()
     {
         $productModel = Mage::getModel('catalog/product');
-        /** @var Mage_Catalog_Model_Resource_Product_Collection $allCollections */
         $allCollections = $productModel->getCollection()
             ->addAttributeToSelect('*')
             ->addAttributeToFilter('type_id', array('in' => array('simple', 'virtual')))
             ->addAttributeToFilter('status', 1);
-        $allCollections->getItems();
-        foreach ($allCollections as $collection) {
-            $this->mockPushItemToQueue($collection);
-        }
-    }
-
-    public function mockPushItemToQueue($item)
-    {
-        $queueModel = Mage::getModel('citrusintegration/queue');
-        $queueCollection = $queueModel->getCollection()->addFieldToSelect('id')
-            ->addFieldToFilter('entity_id', array('in' => array($item->getId(), $item->getIncrementId())))
-            ->addFieldToFilter('type', array('eq' => $item->getResourceName()))
-            ->getFirstItem();
-        if($queueCollection->getData()){
-            $queueModel->load($queueCollection->getId());
-            if($item->getResourceName() == 'sales/order')
-                $queueModel->enqueue($item->getIncrementId(), $item->getResourceName());
-            else
-                $queueModel->enqueue($item->getId(), $item->getResourceName());
-        }else {
-            if($item->getResourceName() == 'sales/order')
-                $queueModel->enqueue($item->getIncrementId(), $item->getResourceName());
-            else
-                $queueModel->enqueue($item->getId(), $item->getResourceName());
-        }
+//        $allCollections->getItems();
+        return $allCollections;
     }
 }
