950bf204deb9eae522b11df1a0baa7cbdfb2d4d1
SIYANG TAO <siyang.tao@citrusad.com>
2018-07-10 14:08:53 +1000

    Add catalogProductCallback test.

--- tests/unit/testsuite/Citrus/Integration/controllers/Citrus_Integration_Controller_Queue_Test.php
+++ tests/unit/testsuite/Citrus/Integration/controllers/Citrus_Integration_Controller_Queue_Test.php
@@ -49,20 +49,89 @@ class Citrus_Integration_Controller_Queue_Test extends TestCase
     {
         $productCollections = $this->getProductsCollection();
         $count = count($productCollections);
-        var_dump($count);
+//        var_dump($count);
 
         $collections = Mage::getModel('citrusintegration/queue')->getCollection();
         foreach ($collections as $item) {
             $item->delete();
         }
         $this->assertEquals(0, Mage::getModel('citrusintegration/queue')->getCollection()->count());
-        var_dump($collections->load()->getSize());
+//        var_dump($collections->load()->getSize());
 
         foreach ($productCollections->getItems() as $collection) {
             $this->model->pushItemToQueue($collection);
         }
 
         $this->assertEquals($count, Mage::getModel('citrusintegration/queue')->getCollection()->count());
+    }
+
+    public function testCatalogProductCallback() {
+        $requestIds = array();
+
+        // Clear queue
+        $collections = Mage::getModel('citrusintegration/queue')->getCollection();
+        foreach ($collections as $item) {
+            $item->delete();
+        }
+
+        // Push 100 products to queue
+        $productCollections = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('type_id', array('in' => array('simple', 'virtual')))
+            ->addAttributeToFilter('status', 1)
+            ->setPageSize(100)
+            ->setCurPage(1);
+        foreach ($productCollections->getItems() as $collection) {
+            $this->model->pushItemToQueue($collection);
+        }
+
+        // Push 100 customers to queue
+        $customerCollections = Mage::getModel(Mage_Customer_Model_Customer::class)->getCollection()
+            ->addAttributeToSelect('*')
+            ->setPageSize(100)
+            ->setCurPage(1);
+        foreach ($customerCollections->getItems() as $collection) {
+            $this->model->pushItemToQueue($collection);
+        }
+
+        // Push 100 orders to queue
+        $orderCollection = Mage::getModel(Mage_Sales_Model_Order::class)->getCollection()
+            ->addAttributeToSelect('*')
+            ->setPageSize(100)
+            ->setCurPage(1);
+        foreach ($orderCollection->getItems() as $collection) {
+            $this->model->pushItemToQueue($collection);
+        }
+
+        // Get request Ids from the queue
+        $queueCollection = Mage::getModel('citrusintegration/queue')->getCollection();
+        $requestIds = $queueCollection->getAllIds();
+
+//        count($requestIds);
+//        var_dump($requestIds);
+
+        // Call CatalogProductCallback()
+        $oldItem = Mage::getModel('citrusintegration/queue')->getCount();
+        $for = count($requestIds)/100;
+        for($i = 0; $i <= $for; $i ++){
+            $tmp_array[] = array_slice($requestIds, $i*100, 100);
+        }
+        array_walk($tmp_array, array($this->model, 'catalogProductCallback'), $oldItem);
+        $newtem = Mage::getModel('citrusintegration/queue')->getCount();
+
+        $session = Mage::getSingleton('adminhtml/session');
+
+        // Assertions here
+        $this->assertEquals(0, $newtem);
+        $this->assertTrue($session->getData('success'));
+
+//        var_dump($session->getData('orderMessage'));
+//        var_dump($session->getData('customerMessage'));
+//        var_dump($session->getData('productMessage'));
+
+        $this->assertEquals(0, $session->getData('orderMessage'));
+        $this->assertEquals(0, $session->getData('customerMessage'));
+        $this->assertEquals(0, $session->getData('productMessage'));
     }
 
     private function getProductsCollection()
