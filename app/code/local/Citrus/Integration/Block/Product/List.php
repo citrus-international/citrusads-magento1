9f97deff53d182656b1a88bfdedde32427289aaa
SIYANG TAO <siyang.tao@citrusad.com>
2018-09-20 08:48:47 +1000

    Performance count() from PHP is very expensive.

--- app/code/local/Citrus/Integration/Block/Product/List.php
+++ app/code/local/Citrus/Integration/Block/Product/List.php
@@ -43,7 +43,7 @@ class Citrus_Integration_Block_Product_List extends Mage_Catalog_Block_Product_L
             $searchAdResponse = Mage::registry('searchAdResponse');
             $collections->getItems();
             $adProductIds = array();
-            $classType = count($collections) > 0 ? get_class($collections->getFirstItem()) : "Mage_Catalog_Model_Product";
+            $classType = $collections->count() > 0 ? get_class($collections->getFirstItem()) : "Mage_Catalog_Model_Product";
             Mage::helper('citrusintegration')->log('Type class: '. $classType, __FILE__, __LINE__);
             if($categoryAdResponse){
                 $adProductIds = $this->getAdResponse($categoryAdResponse['ads'], $collections, $classType);
