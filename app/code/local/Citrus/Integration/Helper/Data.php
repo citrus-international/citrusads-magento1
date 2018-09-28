ad27e6b019dd56bc030724932cc6b8c3c9983468
Kevin Ma <kevin@citrusad.com>
2018-09-28 13:58:59 +1000

    Fix us endpoint

--- app/code/local/Citrus/Integration/Helper/Data.php
+++ app/code/local/Citrus/Integration/Helper/Data.php
@@ -12,7 +12,7 @@ class Citrus_Integration_Helper_Data extends Mage_Core_Helper_Data
 {
     const CITRUS_STAGING_SERVER = "https://staging-integration.citrusad.com/v1/";
     const CITRUS_AU_SERVER = "https://au-integration.citrusad.com/v1/";
-    const CITRUS_US_SERVER = "https://us-integration.citrusad.com/v1/";
+    const CITRUS_US_SERVER = "https://us-west-2-integration.citrusad.com/v1/";
     const MAX_NUMBER_OF_ADS = 3;
     const CITRUS_PAGE_TYPE_SEARCH = 3;
     const CITRUS_PAGE_TYPE_ALL = 0;
